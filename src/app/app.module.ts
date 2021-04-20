286ec9f2797885a55f780e1ac872ee241221f455
joelgrod <joelgrod@gmail.com>
2021-04-20 20:35:47 +0100

    initial socket config added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { 
+  // Server url
+  url: environment.ws_url,
+  options: {} 
+};
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +19,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    SocketIoModule.forRoot(config),
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Sockets
+  ws_url: 'http://localhost:4000'
 };
 
 /*
