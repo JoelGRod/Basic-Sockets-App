94ce3a960b6bd28c164b88344b489aa88f694f09
joelgrod <joelgrod@gmail.com>
2021-04-22 21:03:12 +0100

    auth module, chat module simple-chat and user-list component

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -5,12 +5,16 @@ import { ChatRoutingModule } from './chat-routing.module';
 // Components
 import { MainComponent } from './pages/main/main.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { SimpleChatComponent } from './components/simple-chat/simple-chat.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    ChatComponent
+    ChatComponent,
+    SimpleChatComponent,
+    UserListComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/chat/components/simple-chat/simple-chat.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-chat',
+  templateUrl: './simple-chat.component.html',
+  styles: [
+  ]
+})
+export class SimpleChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
