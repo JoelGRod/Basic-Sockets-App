dab28df64a18538979d59fd0e01cd6f6f9cf4e8e
joelgrod <joelgrod@gmail.com>
2021-06-07 11:58:10 +0100

    dialog service

---
+++ src/app/shared/services/dialog.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+// RXJS
+import { Observable } from 'rxjs';
+// Services
+import { MatDialog } from '@angular/material/dialog';
+// Interfaces
+import { DialogData } from '../interfaces/shared-interfaces';
+// Dialogs
+import { GralDialogComponent } from '../components/gral-dialog/gral-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+
+  constructor(private _dialog: MatDialog) { }
+
+  public openGeneralDialog(data: DialogData): void {
+    const dialogRef = this._dialog.open(GralDialogComponent, {
+      width: '250px',
+      data
+    });
+  }
+
+  public openGeneralDialogResponse(data: DialogData): Observable<boolean> {
+    const dialogRef = this._dialog.open(GralDialogComponent, {
+      width: '250px',
+      data
+    });
+
+    return dialogRef.afterClosed();
+  }
+}
