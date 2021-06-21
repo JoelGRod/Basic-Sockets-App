740f7d2cce071665d3f2907bf0475f424cfead0f
joelgrod <joelgrod@gmail.com>
2021-06-21 22:28:17 +0100

    about component

--- src/app/home/pages/about/about.component.ts
+++ src/app/home/pages/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-about',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _matIconRegistry: MatIconRegistry,
+    private _domSanitizer: DomSanitizer
+  ) { 
+    this._matIconRegistry.addSvgIcon(
+      `twitter`,
+      this._domSanitizer.bypassSecurityTrustResourceUrl("../../../../assets/images/icons/twitter.svg")
+    );
+    this._matIconRegistry.addSvgIcon(
+      `linkedin`,
+      this._domSanitizer.bypassSecurityTrustResourceUrl("../../../../assets/images/icons/linkedin.svg")
+    );
+  }
 
   ngOnInit(): void {
   }
