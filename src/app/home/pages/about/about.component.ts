130d19ddb02b5156e2c76f237dfcf5a995743961
joelgrod <joelgrod@gmail.com>
2021-06-22 15:25:54 +0100

    about component

--- src/app/home/pages/about/about.component.ts
+++ src/app/home/pages/about/about.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { MatIconRegistry } from "@angular/material/icon";
-import { DomSanitizer } from "@angular/platform-browser";
+import { Component } from '@angular/core';
 import { IconRegistryService } from 'src/app/shared/services/icon-registry.service';
+
+export interface Section {
+  name: string;
+  updated: Date;
+}
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
   constructor(
     private _iconRegistryService: IconRegistryService
   ) {}
 
-  ngOnInit(): void {
-  }
+  folders: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  notes: Section[] = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    }
+  ];
 
 }
