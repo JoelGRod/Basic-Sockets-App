240d108bc3848479830d96bf35fe9e23ee6deecd
joelgrod <joelgrod@gmail.com>
2021-06-25 17:54:49 +0100

    chip list component - home

--- src/app/home/components/chips-list/chips-list.component.ts
+++ src/app/home/components/chips-list/chips-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+// Interfaces
+import { Skill } from '../../interfaces/home-interfaces';
 
 @Component({
   selector: 'app-chips-list',
   templateUrl: './chips-list.component.html',
   styleUrls: ['./chips-list.component.scss']
 })
-export class ChipsListComponent implements OnInit {
+export class ChipsListComponent {
+
+  @Input() list_title: string = 'Main';
+  @Input() chip_list: Skill[] = [];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
