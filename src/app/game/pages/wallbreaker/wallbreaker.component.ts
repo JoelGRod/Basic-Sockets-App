701fc2f2d11a3d7a40ae25e5edee201701f9788b
joelgrod <joelgrod@gmail.com>
2021-07-02 12:23:28 +0100

    game loader used in wallbreaker page

--- src/app/game/pages/wallbreaker/wallbreaker.component.ts
+++ src/app/game/pages/wallbreaker/wallbreaker.component.ts
@@ -1,23 +1,12 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
-declare var UnityLoader: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-wallbreaker',
   templateUrl: './wallbreaker.component.html',
   styleUrls: ['./wallbreaker.component.scss']
 })
-export class WallbreakerComponent implements AfterViewInit, OnDestroy {
-
-  unityInstance: any;
+export class WallbreakerComponent {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-    this.unityInstance = UnityLoader.instantiate("unityContainer", "assets/games/wallbreaker/wallbreaker.json");
-  }
-
-  ngOnDestroy(): void {
-    this.unityInstance.Quit( () => { console.log('done') } );
-  }
 
 }
