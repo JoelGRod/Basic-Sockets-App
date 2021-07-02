0dd0ed0ddcffd485fb06298552604efbea088049
joelgrod <joelgrod@gmail.com>
2021-07-02 12:25:50 +0100

    game loader used in tetris page

--- src/app/game/pages/tetris/tetris.component.ts
+++ src/app/game/pages/tetris/tetris.component.ts
@@ -1,23 +1,12 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
-declare var UnityLoader: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tetris',
   templateUrl: './tetris.component.html',
   styleUrls: ['./tetris.component.scss']
 })
-export class TetrisComponent implements AfterViewInit, OnDestroy {
-
-  unityInstance: any;
+export class TetrisComponent {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-    this.unityInstance = UnityLoader.instantiate("unityContainer", "assets/games/tetris/tetris.json");
-  }
-
-  ngOnDestroy(): void {
-    this.unityInstance.Quit( () => { console.log('done') } );
-  }
 
 }
