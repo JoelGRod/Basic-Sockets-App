166f5c06d08cd3429713c251ecaf75be6ed0b087
joelgrod <joelgrod@gmail.com>
2021-07-02 12:33:42 +0100

    game loader used in running girl page

--- src/app/game/pages/running/running.component.ts
+++ src/app/game/pages/running/running.component.ts
@@ -1,23 +1,12 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
-declare var UnityLoader: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-running',
   templateUrl: './running.component.html',
   styleUrls: ['./running.component.scss']
 })
-export class RunningComponent implements AfterViewInit, OnDestroy {
-
-  unityInstance: any;
+export class RunningComponent {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-    this.unityInstance = UnityLoader.instantiate("unityContainer", "assets/games/runninggirl/runninggirl.json");
-  }
-
-  ngOnDestroy(): void {
-    this.unityInstance.Quit( () => { console.log('done') } );
-  }
 
 }
