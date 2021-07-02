de7a378e2692b577a7af44990157fbf1a2cf5be2
joelgrod <joelgrod@gmail.com>
2021-07-02 12:17:40 +0100

    game loader used in minesweeper page

--- src/app/game/pages/minesweeper/minesweeper.component.ts
+++ src/app/game/pages/minesweeper/minesweeper.component.ts
@@ -1,23 +1,12 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
-declare var UnityLoader: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-minesweeper',
   templateUrl: './minesweeper.component.html',
   styleUrls: ['./minesweeper.component.scss']
 })
-export class MinesweeperComponent implements AfterViewInit, OnDestroy {
-
-  unityInstance: any;
+export class MinesweeperComponent {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-    this.unityInstance = UnityLoader.instantiate("unityContainer", "assets/games/minesweeper/minesweeper.json");
-  }
-
-  ngOnDestroy(): void {
-    this.unityInstance.Quit( () => { console.log('done') } );
-  }
 
 }
