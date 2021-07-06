74bfa47796fad68450216a2a4abbf5fe3c6fc5ac
joelgrod <joelgrod@gmail.com>
2021-07-06 20:07:22 +0100

    contact form component functionality

--- src/app/home/components/contact-form/contact-form.component.ts
+++ src/app/home/components/contact-form/contact-form.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// Services
+import { FormValidatorsService } from 'src/app/shared/validators/form-validators.service';
+import { HomeService } from '../../services/home.service';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
 
-  constructor() { }
+  public form: FormGroup = this._fb.group({
+    name: ['', [
+      Validators.required,
+      Validators.minLength(3)
+    ]],
+    contact_email: ['', [
+      Validators.required,
+      Validators.pattern(this._fvalidators.email_pattern)
+    ]],
+    subject: ['', [
+      Validators.required,
+      Validators.minLength(3)
+    ]],
+    msg: ['', [
+      Validators.required,
+      Validators.minLength(3)
+    ]]
+  });
+  
+  constructor( 
+    private _fb: FormBuilder,
+    private _fvalidators: FormValidatorsService,
+    private _home_service: HomeService
+  ) { }
 
-  ngOnInit(): void {
+  send_message(): void {
+
+    Object.keys(this.form.controls).forEach((key) => this.form.get(key)!.setValue(this.form.get(key)!.value.trim()));
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this._home_service.send_contact_email(this.form.value)
+      .subscribe( resp => {
+        this.form.reset();
+      });
+
   }
 
 }
